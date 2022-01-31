ad95c3bc719de27dbda5b38d11c2f541caf19aa9
Amit <amit840901@gmail.com>
2022-01-31 13:39:55 +0530

    Initial Client Setup,Landing Page and React Router

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Landing from './pages/Landing';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Dashboard</div>} />
+        <Route path='/register' element={<div>Register</div>} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<h1>Error</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
