397a3c129c196e94e4aeb9248b21bf65eb417d38
Amit <amit840901@gmail.com>
2022-01-31 14:14:05 +0530

    Set up Error Page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className='fullpage'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>Ohh! page not found</h3>
+        <p>We Can't seem to find the page you're looking for</p>
+        <Link to='/'>Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
