32ba1d9112137f8416038bc1411fe6f825bee52e
Amit <amit840901@gmail.com>
2022-01-31 17:48:48 +0530

    Separate input Component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Register</h3>
+        {/* name Input */}
+        <FormRow
+          type='text'
+          name='name'
+          handleChange={handleChange}
+          value={values.name}
+        />
+        {/* email input */}
+        <FormRow
+          type='email'
+          name='email'
+          handleChange={handleChange}
+          value={values.email}
+        />
+
+        {/* password input */}
+        <FormRow
+          type='password'
+          name='password'
+          handleChange={handleChange}
+          value={values.password}
+        />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
