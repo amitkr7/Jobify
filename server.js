42c1c8bcbe08b24591a04dd2c402a85beb3a6a5e
Amit <amit840901@gmail.com>
2022-02-02 13:03:56 +0530

    Setup server and errorHandler middleware

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+//middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
