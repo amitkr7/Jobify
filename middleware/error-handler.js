9d0e792d16aaa17f75d11bb11ce4c9610ce86463
Amit <amit840901@gmail.com>
2022-02-02 21:40:58 +0530

    Setup express-async-error Handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,12 @@
+import { StatusCodes } from 'http-status-codes';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: 'There was an error' });
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: 'Something Went Wrong, Try Again Later',
+  };
+  res.status(defaultError.statusCode).json({ msg: err });
 };
 
 export default errorHandlerMiddleware;
