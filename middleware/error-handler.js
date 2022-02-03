2f604b9f29028bfce479644ee73af8024d098e6a
Amit <amit840901@gmail.com>
2022-02-03 15:41:24 +0530

    CustomAPI error class to handle custom error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something Went Wrong, Try Again Later',
   };
 
