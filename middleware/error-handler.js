f84a8d7b4573425624ce27b2bfc5bfa71f2097ec
Amit <amit840901@gmail.com>
2022-02-03 14:37:24 +0530

    Handle missing and invalid field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,18 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: 'Something Went Wrong, Try Again Later',
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
