2dca756d731c108a00127a539cfd9d85dcf5cfb9
Amit <amit840901@gmail.com>
2022-02-03 18:18:59 +0530

    Hash password before saving

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide name'],
@@ -36,4 +37,9 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-export default mongoose.model('User', userSchema);
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+export default mongoose.model('User', UserSchema);
