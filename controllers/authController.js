60ab38d68e0a7905440bb13d37da4ed282ccb0a8
Amit Kumar67094 <ak67094@corp.cyient.com>
2022-05-31 21:59:36 +0530

    Login Controller Backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 import { StatusCodes } from 'http-status-codes';
 
 const register = async (req, res) => {
@@ -30,7 +30,26 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  return res.send('Login');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    throw new UnauthenticatedError(`User with ${email} does not exist`);
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
 const updateUser = async (req, res) => {
