84d31b06aaa7c05c8f68b7fffd2f9e2e99fe68f9
Amit Kumar67094 <ak67094@corp.cyient.com>
2022-06-17 01:16:43 +0530

    Setup axios Interceptor

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -36,6 +36,35 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+  });
+
+  //request
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers.common['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  //response
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR');
+      }
+      return Promise.reject(error);
+    }
+  );
+
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
     clearAlert();
@@ -103,9 +132,15 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage();
   };
 
-  const updateUser = (currentUser) => {
-    console.log(currentUser);
+  const updateUser = async (currentUser) => {
+    try {
+      const { data } = await authFetch.patch('/auth/updateUser', currentUser);
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
+
   const toggleSidebar = () => {
     dispatch({ type: TOGGLE_SIDEBAR });
   };
