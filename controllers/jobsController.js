786623cf48e9b29f30930312cbb01a74ff7bd276
amitkr7 <ak67094@corp.cyient.com>
2022-12-03 23:15:43 +0530

    Show Stats Functionality

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -73,7 +73,20 @@ const showStats = async (req, res) => {
     { $group: { _id: '$status', count: { $sum: 1 } } },
   ]);
 
-  res.status(StatusCodes.OK).json({ stats });
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = [];
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, getAllJobs, deleteJob, updateJob, showStats };
