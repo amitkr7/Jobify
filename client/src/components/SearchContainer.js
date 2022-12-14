04155b544b32babb2d5244038b85f71aaa7a915c
amitkr7 <amit840901@gmail.com>
2022-12-14 23:07:54 +0530

    Search with Debounce

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import { FormRow, FormRowSelect } from '.';
 import { useAppContext } from '../context/appContext';
 import Wrapper from '../assets/wrappers/SearchContainer';
+import { useState, useMemo } from 'react';
 
 const SearchContainer = () => {
+  const [localSearch, setLocalSearch] = useState('');
   const {
     isLoading,
     search,
@@ -24,8 +25,22 @@ const SearchContainer = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocalSearch('');
     clearFilters();
   };
+
+  const debounce = () => {
+    let timeoutId;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        handleChange({ name: e.target.name, value: e.target.value });
+      }, 1000);
+    };
+  };
+
+  const optimizedDebounce = useMemo(() => debounce(), []);
 
   return (
     <Wrapper>
@@ -36,8 +51,8 @@ const SearchContainer = () => {
           <FormRow
             type='text'
             name='search'
-            value={search}
-            handleChange={handleSearch}></FormRow>
+            value={localSearch}
+            handleChange={optimizedDebounce}></FormRow>
 
           {/* search by status */}
           <FormRowSelect
